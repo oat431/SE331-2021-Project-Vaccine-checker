cfc3f3b33c6ca2bbcc8ed0438cee731a6518613d
Oralita <oat431@gmail.com>
2021-08-13 21:08:08 +0700

    [🍦 add] : person infomation

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import About from '../pages/About.vue'
 import VaccinatedInfo from '../pages/VaccinatedInfo.vue'
-// import vaccineAPI from '../services/vaccineAPI.js'
-
+import NProgress from 'nprogress'
+import vaccineAPI from '../services/vaccineAPI.js'
+import GlobalState from '../store/index'
 
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Home,
-        props: (route) => ({page: parseInt(route.query.page) || 1})
+        props: (route) => ({ page: parseInt(route.query.page) || 1 }),
     },
     {
         path: '/about',
@@ -21,13 +22,29 @@ const routes = [
     {
         path: '/vaccinated/:id',
         name: 'Vaccinated',
-        component: VaccinatedInfo
-    }
+        component: VaccinatedInfo,
+        props: true,
+        beforeEnter: (to) => {
+            return vaccineAPI.getVaccinatedPerson(to.params.id).then((res) => {
+                GlobalState.vaccinatedPerson = res.data
+            }).catch((err)=>{
+                console.log(err)
+            })
+        },
+    },
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+})
+
+router.beforeEach(() => {
+    NProgress.start()
+})
+
+router.beforeEach(() => {
+    NProgress.done()
 })
 
 export default router
