97c16717b81cef36d5c16eac612cf053a9e2e946
Oralita <oat431@gmail.com>
2021-08-14 21:15:41 +0700

    [🥧 add] : doctor comment component

---
+++ src/components/DoctorComment.vue
@@ -0,0 +1,29 @@
+<script>
+import dayjs from 'dayjs'
+export default {
+    props:['doctor'],
+    data(){
+        return {
+            date:''
+        }
+    },
+
+    created() {
+        const d = dayjs(this.doctor.comment_date);
+        this.date = d.format('dddd MMMM D, YYYY');
+    }
+}
+</script>
+<template>
+        <div class="flex max-w-5xl mx-auto overflow-hidden bg-white rounded-lg shadow-lg">
+        <img class="w-1/5 bg-cover" :src="doctor.comment_pic" />
+
+        <div class="w-4/5 p-4 md:p-4">
+            <h1 class="text-2xl font-bold text-gray-800">{{doctor.comment_by}}</h1>
+
+            <p class="mt-5 text-lg font-medium text-gray-600">{{doctor.comment}}</p>
+
+            <p class="mt-5 text-sm font-normal text-gray-700">comment on : {{this.date}}</p>
+        </div>
+    </div>
+</template>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,15 @@
 <script>
 export default {
-   data(){
+    inject:['GlobalState'],
+    data(){
        return {
            toggle:false,
            doctors:[
-               "Dr. Roger",
-               "Dr. Lipton",
-               "Dr. SomChai",
-               "Dr. Lillie",
-               "Dr. Light"
+               "Roger",
+               "Lipton",
+               "SomChai",
+               "Lillie",
+               "Light"
            ]
        }
    }, 
@@ -21,35 +22,64 @@ export default {
 </script>
 <template>
     <nav class="bg-white shadow dark:bg-gray-800">
-        <div class="container px-6 py-4 mx-auto">
-            <div class="md:flex md:items-center md:justify-between">
+        <div  class="container px-6 py-4 mx-auto">
+            <div v-if="GlobalState.isdoctor" class="md:flex md:items-center md:justify-between">
+                
+                <div class="flex items-center justify-between">
+                    <div class="text-xl font-semibold text-gray-700">
+                        <router-link class="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" :to="{name:'Doctor',params:{name:GlobalState.doctorName}}">SE331</router-link>
+                    </div>
+                </div>
+
+                <div class="flex-1 md:flex md:items-center md:justify-between">
+                    <div class="flex flex-row -mx-4 md:flex-row md:items-center md:mx-8">
+                        <router-link :to="{name:'Doctor',params:{name:GlobalState.doctorName}}" class="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 hover:bg-gray-300">Home</router-link>
+                        <router-link to="/about" class="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 hover:bg-gray-300">About</router-link>
+                    </div>
+
+                    <div class="flex items-right mt-4 md:mt-0 relative">
+                        <button class="relative z-10 block p-2 bg-white rounded-md dark:bg-gray-800" @click="show">
+                            Welcome Doctor. {{GlobalState.doctorName}} 
+                        </button>
+                        <div v-if="toggle" class="absolute right-0 z-20 w-48 py-4 md:mt-10 bg-white rounded-md shadow-xl dark:bg-gray-800">
+                            <router-link to="/" @click="show" 
+                            class="block px-4 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-blue-500 hover:text-white dark:hover:text-white">
+                                Logout 
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div v-else class="md:flex md:items-center md:justify-between">
+                
                 <div class="flex items-center justify-between">
                     <div class="text-xl font-semibold text-gray-700">
                         <router-link class="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" to="/">SE331</router-link>
                     </div>
                 </div>
 
-                <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
                 <div class="flex-1 md:flex md:items-center md:justify-between">
                     <div class="flex flex-row -mx-4 md:flex-row md:items-center md:mx-8">
                         <router-link to="/" class="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 hover:bg-gray-300">Home</router-link>
                         <router-link to="/about" class="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 hover:bg-gray-300">About</router-link>
-                        
                     </div>
 
                     <div class="flex items-right mt-4 md:mt-0 relative">
                         <button class="relative z-10 block p-2 bg-white rounded-md dark:bg-gray-800" @click="show">
                            Doctor Login 
                         </button>
-                        <div v-if="toggle" class="absolute right-0 z-20 w-48 py-4 md:mt-60 bg-white rounded-md shadow-xl dark:bg-gray-800">
-                            <router-link v-for="doctor in doctors" :key="doctor" to="/doctor" @click="show" 
+                        <div v-if="toggle" class="absolute right-0 z-20 w-48 py-4 md:mt-10 bg-white rounded-md shadow-xl dark:bg-gray-800">
+                            <router-link v-for="doctor in doctors" :key="doctor" :to="{name:'Doctor',params:{name:doctor}}" @click="show" 
                             class="block px-4 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-blue-500 hover:text-white dark:hover:text-white">
-                                 Login as {{ doctor }} 
+                                 Login as Dr. {{ doctor }} 
                             </router-link>
                         </div>
                     </div>
                 </div>
+
             </div>
+
         </div>
     </nav>
 </template>
